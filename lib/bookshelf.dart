cd7d4a7112380a704fce77383ff1bdf07afc25b4
CoMatu <30466272+CoMatu@users.noreply.github.com>
2018-11-01 18:29:36 +0500

    сделал переход между экранами сдвиг влево ++

--- lib/bookshelf.dart
+++ lib/bookshelf.dart
@@ -1,10 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:toddler_books/bookshelf_card.dart';
+import 'package:flutter/animation.dart';
 
-class BookShelf extends StatelessWidget {
+class BookShelf extends StatefulWidget {
+  @override
+  BookShelfState createState() {
+    return BookShelfState();
+  }
+}
+
+class BookShelfState extends State<BookShelf> with TickerProviderStateMixin {
+
+  AnimationController _controller;
+  Animation _animation;
+
+  @override
+  void initState(){
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 3));
+    _animation = Tween(begin: 1.0, end: 0.0).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.fastOutSlowIn,
+    ));
+    _controller.forward();
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+
+    final double width = MediaQuery.of(context).size.width;
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (BuildContext context, Widget child) {
+        double animval = _animation.value;
+        return Scaffold(
+          body:
+          Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/background.png'),
+                      fit: BoxFit.fill),
+                ),
+              ),
+              Center(child: BookshelfCard()),
+              Transform(
+                transform: Matrix4.translationValues(animval * width, 0.0, 0.0),
+                child: Container(
+                  width: width,
+                  color: Colors.transparent,
+                  child:
+                  Image(image: AssetImage('assets/images/surround.png',),
+                    fit: BoxFit.fill,
+                  )
+                )
+              )
+            ],
+          ),
+
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+/*
     return Scaffold(
       body: Stack(
           children: <Widget>[
@@ -20,6 +90,5 @@ class BookShelf extends StatelessWidget {
         ),
 
     );
-  }
 
-}
+ */
--- lib/bookshelf_card.dart
+++ lib/bookshelf_card.dart
@@ -1,29 +1,46 @@
 import 'package:flutter/material.dart';
 
 class BookshelfCard extends StatelessWidget {
+  String assetName = 'assets/images/start_background.png';
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    Radius radius = Radius.circular(10.0);
     return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(radius)
+      ),
       color: Colors.transparent,
       key: key,
+child: Container(
+  width: 280.0,
+  height: 200.0,
+),
+/*
       child: SizedBox(
         height: 200.0,
         width: 280.0,
         child: Container(
               decoration: BoxDecoration(
                 border: Border.all(width: 10.0, color: Colors.amber),
-                borderRadius: BorderRadius.all(Radius.circular(15.0)),
+//                borderRadius: BorderRadius.all(Radius.circular(15.0)),
               ),
               child: Container(
                 decoration: BoxDecoration(
                   border: Border.all(width: 7.0, color: Colors.brown[500]),
-                  borderRadius: BorderRadius.circular(5.0)
+//                  borderRadius: BorderRadius.circular(5.0)
+                ),
+                child: Image(
+                  //TODO загрузка картинки из Firebase
+                    image: AssetImage(assetName),
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
 
       )
+*/
     );
   }
 
