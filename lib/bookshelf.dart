77364764ab3f881a3104090f36e482ccfc8c0872
CoMatu <30466272+CoMatu@users.noreply.github.com>
2018-11-02 19:47:32 +0500

    сделал листвью

--- lib/bookshelf.dart
+++ lib/bookshelf.dart
@@ -10,12 +10,11 @@ class BookShelf extends StatefulWidget {
 }
 
 class BookShelfState extends State<BookShelf> with TickerProviderStateMixin {
-
   AnimationController _controller;
   Animation _animation;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _controller =
         AnimationController(vsync: this, duration: Duration(seconds: 3));
@@ -24,53 +23,53 @@ class BookShelfState extends State<BookShelf> with TickerProviderStateMixin {
       curve: Curves.fastOutSlowIn,
     ));
     _controller.forward();
-
-
   }
 
   @override
   Widget build(BuildContext context) {
-
     final double width = MediaQuery.of(context).size.width;
-    return AnimatedBuilder(
-      animation: _controller,
-      builder: (BuildContext context, Widget child) {
-        double animval = _animation.value;
-        return Scaffold(
-          body:
-          Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/background.png'),
-                      fit: BoxFit.fill),
-                ),
-              ),
-              Transform(
-                transform: Matrix4.translationValues(animval * width, 0.0, 0.0),
-                child: Container(
-                  width: width,
-                  color: Colors.transparent,
-                  child:
-                  Image(image: AssetImage('assets/images/surround.png',),
-                    fit: BoxFit.fill,
-                  )
-                )
-              ),
-              Center(
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 5,
-                    itemBuilder: (context, index) {
-                      return BookshelfCard();
-                    },
-                  )
-              ),
-            ],
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/background.png'),
+                  fit: BoxFit.fill),
+            ),
           ),
-        );
-      },
+          AnimatedBuilder(
+            animation: _controller,
+            builder: (BuildContext context, Widget child) {
+              double animval = _animation.value;
+              return Transform(
+                  transform: Matrix4.translationValues(animval * width, 0.0, 0.0),
+                  child: Container(
+                      width: width,
+                      color: Colors.transparent,
+                      child:
+                      Image(image: AssetImage('assets/images/surround.png',),
+                        fit: BoxFit.fill,
+                      )
+                  )
+              );
+            },
+          ),
+          Center(
+            child: Container(
+              height: 200.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return BookshelfCard();
+                },
+              )
+              ,
+            ),
+          )
+        ],
+      ),
     );
   }
 
